285473e3da931b6efc23977ada00e38593da1f18
Michael-Tejano <112233618+M1chael30@users.noreply.github.com>
2026-07-11 18:30:11 +0800

    update: changed the Metadata title

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: "Michael Tejano | Full Stack Junior Developer",
+  title: "Michael Tejano",
   description: "Portfolio website of Michael Tejano, a full stack junior developer.",
   keywords: [
     "Michael Tejano",
